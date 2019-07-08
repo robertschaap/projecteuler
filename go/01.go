8d428612cac72603746c5d459e45aa8e73a5143a
robertschaap <robert.schaap@mac.com>
2019-07-08 21:52:27 +0200

    01: Sum of divisible by 3 or 5:Go

---
+++ go/01.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sum1() int {
+	sum := 0
+
+	for i := 0; i < 1000; i++ {
+		if i % 3 == 0 || i % 5 == 0 {
+			sum += i
+		}
+	}
+
+	return sum
+}
+
+func sum2() int {
+	var sum int
+	var a [1000]int
+
+	for i := range a {
+		if i % 3 == 0 || i % 5 == 0 {
+			sum += i
+		}
+	}
+
+	return sum
+}
+
+func main() {
+	// fmt.Println(sum1())
+	fmt.Println(sum2())
+}
