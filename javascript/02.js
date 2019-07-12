36ac097a541de68f554f139b6ad2a11cba689234
robertschaap <robert.schaap@mac.com>
2019-07-12 22:21:57 +0200

    02: fibonacci even sum: Javascript

---
+++ javascript/02.js
@@ -0,0 +1,13 @@
+function fib() {
+  let sum = [1, 2];
+
+  while (true) {
+    const inc = sum[sum.length-1] + sum[sum.length-2];
+    if (inc > 4000000) break;
+    sum.push(inc)
+  }
+
+  return sum.reduce((acc , val) => val % 2 == 0 ? acc + val : acc);
+}
+
+console.log(fib());
